d7e723080f3416ba8fc056ac42b89c88e9c86974
zhicheng ran <307853639@qq.com>
2023-02-24 17:28:45 +0800

    add vue type

--- test/index.ts
+++ test/index.ts
@@ -1,17 +1,21 @@
 /*
  * @Author: zhicheng ran
  * @Date: 2023-02-22 16:15:56
- * @LastEditTime: 2023-02-24 17:18:17
+ * @LastEditTime: 2023-02-24 17:28:30
  * @FilePath: \pic-processing\test\index.ts
  * @Description:
  */
 import { PicProcessor, OpenCV } from '../src'
-
-const vue = new (window as any).Vue({
+import Vue from 'vue'
+const vue = new (
+  window as {
+    Vue: typeof Vue
+  }
+).Vue({
   el: '#app',
   data: {
     config: {
-      content: undefined,
+      content: '',
       rotate: 0,
       scale: 1,
     },
@@ -21,16 +25,18 @@ const vue = new (window as any).Vue({
   methods: {
     async handleFileChange(e: any) {
       const file = e.target!.files[0]
-      ;(this as any).config.content = file
+      this.config.content = file
     },
   },
   watch: {
     config: {
       async handler() {
-        const { config, processor } = this as any
-        ;(this as any).pic = (
-          await (processor as PicProcessor).render(config)
-        ).base64
+        if (this.config.content) {
+          const { config, processor } = this
+          this.pic = (
+            await (processor as PicProcessor).render(config)
+          ).base64
+        }
       },
       deep: true,
     },
